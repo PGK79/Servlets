e06b0d11651177ccf43851009a2cd45fd46f5709
PGK79 <pgk_nev@mail.ru>
2022-12-28 23:49:42 +0500

    Добавлена проверка на некорректный id в set() и getById

--- src/main/java/ru/netology/service/PostService.java
+++ src/main/java/ru/netology/service/PostService.java
@@ -4,29 +4,38 @@ import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 import ru.netology.repository.PostRepository;
 
-import java.util.List;
+import java.util.Map;
 
 public class PostService {
-  private final PostRepository repository;
+    private final PostRepository repository;
 
-  public PostService(PostRepository repository) {
-    this.repository = repository;
-  }
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
 
-  public List<Post> all() {
-    return repository.all();
-  }
+    public Map<Long, Post> all() {
+        return repository.all();
+    }
 
-  public Post getById(long id) {
-    return repository.getById(id).orElseThrow(NotFoundException::new);
-  }
+    public Post getById(long id) {
+        if (id <= all().size()) {
+            return repository.getById(id).orElseThrow(NotFoundException::new);
+        } else { // профилактика NotFoundException при существующих id
+            return new Post(0, "Введено не корректное ID. Измените запрос, "
+                    + "используйте диапазон id от 1 до " + all().size());
+        }
+    }
 
-  public Post save(Post post) {
-    return repository.save(post);
-  }
+    public Post save(Post post) {
+        if (post.getId() <= all().size()) {
+            return repository.save(post);
+        } else {
+            return new Post(0, "Введено не корректное ID. Измените запрос, "
+                    + "используйте диапазон id от 1 до " + all().size());
+        }
+    }
 
-  public void removeById(long id) {
-    repository.removeById(id);
-  }
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
 }
-
